fd53873adb1541d9cadcb2eb62048d9846e16971
Kevin Cantu <kevincantu@gmail.com>
2011-01-07 05:02:06 -0800

    cleanup

--- demand.clj
+++ demand.clj
@@ -2,15 +2,14 @@
 
 (def filename (first *command-line-args*))
 (use '[clojure.java.io :only [input-stream]]) 
-(def s (input-stream filename)) 
+(def stream (input-stream filename)) 
 
-(defn hmm [n] 
-   
-   (def b (.read ^java.io.BufferedInputStream s)) 
+(defn slowjure [n] 
+   (def b (.read ^java.io.BufferedInputStream stream)) 
    (cond
       (== b -1)
          n
       :else
          (recur (bit-xor n b))))
 
-(println "Clojure XOR of bytes: " (hmm 0))
+(println "Clojure XOR of bytes: " (slowjure 0))
