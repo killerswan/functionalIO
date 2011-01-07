75257e3d73e851c596b15dbab076ea4d0d210488
Kevin Cantu <kevincantu@gmail.com>
2011-01-07 04:34:08 -0800

    before type annoatation

--- demand.clj
+++ demand.clj
@@ -1,19 +1,21 @@
 
 ;OMG this is slow
 
-(set! *warn-on-reflection* true)
+;(set! *warn-on-reflection* true)
 
 (def filename (first *command-line-args*))
 (use '[clojure.java.io :only [input-stream]]) 
 (def s (input-stream filename)) 
-; (def s (input-stream "/code/bin/godoc"))     
+;(def s (input-stream "/code/bin/godoc"))     
 
-(defn ^int hmm [n] 
-   (def b ^int (^int .read s)) 
+(defn hmm [n] 
+   
+   (def b (.read s)) 
    (cond
-      (<= b -1)
+      (== b -1)
          n
       :else
-         (recur ^int (bit-xor n b))))
+         ;(recur n)))
+         (recur (bit-xor n b))))
 
-(println "Clojure XOR of bytes (demand?): " (hmm 0))
+(println "Clojure XOR of bytes: " (hmm 0))
